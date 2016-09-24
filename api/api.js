e00faf76be6798f242b46f983f58e7900807de0a
Thomas Klein <tklein@solstice-consulting.com>
2016-09-24 11:19:55 -0500

    updates for wit requests

---
+++ api/api.js
@@ -0,0 +1,15 @@
+const request = require('request-promise');
+
+module.exports = {
+    callAPI: callAPI
+}
+
+function callAPI(zipcode) {
+    request()
+    .then(function(data) {
+
+    })
+    .catch(function(err) {
+
+    });
+}
